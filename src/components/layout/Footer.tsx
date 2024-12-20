2160b7322800dcc8e0a07f18b9b456073336a4a8
Mukul Kumar <mukul.kumar630@gmail.com>
2024-12-20 15:05:44 +0530

    Initial commit

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import style from "./style.module.css";
+
+const Footer = () => {
+    return (
+        <div className={style.footer}>
+            <div className={style.footerwrap}>
+                Made by Mukul!!!
+            </div>
+        </div >
+    )
+}
+
+export default Footer;
